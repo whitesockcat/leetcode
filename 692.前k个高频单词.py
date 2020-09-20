4d94a9e4c50b7049aad8f8ba441b051f2eb7a387
whitesockcat <1803744718@qq.com>
2020-09-20 17:41:57 +0800

    1

--- 692.前k个高频单词.py
+++ 692.前k个高频单词.py
@@ -8,16 +8,14 @@ import heapq
 # @lc code=start
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        # # 会报凑
-        # count = collections.Counter(words)
-        # candidates = count.keys()
-        # candidates.sort(key = lambda w: (-count[w], w))
-        # return candidates[:k]
-
         count = collections.Counter(words)
-        heap = [(-freq, word) for word, freq in count.items()]
-        heapq.heapify(heap)
-        return [heapq.heappop(heap)[1] for _ in range(k)]
+        s_count = sorted(count.items(), key=lambda x: (-x[1],x[0]))
+        return [s_count.pop(0)[0] for _ in range(k)]
+
+        # count = collections.Counter(words)
+        # heap = [(-freq, word) for word, freq in count.items()]
+        # heapq.heapify(heap)
+        # return [heapq.heappop(heap)[1] for _ in range(k)]
 
 # @lc code=end
 
